d4bd8ea1e5bf126c22e1397970b900fcea83a94f
Woody Lin <wancheng.lin001@gmail.com>
2026-01-24 14:37:47 -0500

    Update GameCanvas.tsx

--- pixel-fish-miner/components/GameCanvas.tsx
+++ pixel-fish-miner/components/GameCanvas.tsx
@@ -1545,10 +1545,44 @@ const GameCanvas: React.FC<GameCanvasProps> = ({
       ctx.fillStyle = "#212121"; // Black Shirt
       ctx.fillRect(2, -45, 16, 25);
 
-      // Arms
+      // Left Arm (Skin)
       ctx.fillStyle = "#ffccbc";
-      ctx.fillRect(-6, -42, 6, 20);
-      ctx.fillRect(20, -42, 6, 20);
+      ctx.fillRect(-6, -42, 6, 16);
+
+      // Hook Hand (Left)
+      ctx.fillStyle = "#90a4ae"; // Metal grey
+      // Hook shaft
+      ctx.fillRect(-6, -26, 6, 4);
+      // Hook curve
+      ctx.beginPath();
+      ctx.fillStyle = "#90a4ae";
+      ctx.fillRect(-8, -24, 2, 6);
+      ctx.fillRect(-10, -18, 4, 2);
+      ctx.fillRect(-8, -20, 2, 2);
+      // Hook point
+      ctx.fillStyle = "#eceff1"; // Shiny tip
+      ctx.fillRect(-10, -18, 2, 1);
+
+      // Right Arm (Skin)
+      ctx.fillStyle = "#ffccbc";
+      ctx.fillRect(20, -42, 6, 16);
+
+      // Pirate Pistol (Right hand)
+      // Handle (Wood)
+      ctx.fillStyle = "#5d4037";
+      ctx.fillRect(24, -25, 4, 6);
+      // Barrel (Metal)
+      ctx.fillStyle = "#546e7a";
+      ctx.fillRect(28, -24, 8, 4);
+      // Barrel tip
+      ctx.fillStyle = "#37474f";
+      ctx.fillRect(36, -24, 2, 4);
+      // Hammer
+      ctx.fillStyle = "#78909c";
+      ctx.fillRect(26, -26, 2, 2);
+      // Gold trim on handle
+      ctx.fillStyle = "#fdd835";
+      ctx.fillRect(24, -23, 4, 1);
 
       // Head
       ctx.fillStyle = "#ffccbc";
